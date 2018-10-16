944584976b58ebf641a9687cf85b83ad3450a178
liwudi <liwudi@idvert.com>
2018-10-16 12:03:08 +0800

    一个发布订阅模式的实现

---
+++ libs/index.js
@@ -0,0 +1,82 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+/**
+ * Created by mapbar_front on 2018/10/16
+ */
+function PubSub() {
+    this.handles = {
+        eventName: {
+            eventsList: [],
+            isOne: false
+        }
+    };
+}
+PubSub.prototype.subscribe = function (eventName, callback) {
+    var EventsList = [];
+    if (arguments.length < 2) {
+        throw new TypeError('arguments error');
+    }
+    if (Reflect.has(this.handles, eventName)) {
+        EventsList = this.handles[eventName].eventsList;
+    } else {
+        this.handles[eventName] = {
+            eventsList: [callback],
+            isOne: false
+        };
+    }
+    EventsList.push(callback);
+};
+PubSub.prototype.subscribeOne = function (eventName, callback) {
+    var EventsList = [];
+    if (arguments.length < 2) {
+        throw new TypeError('arguments error');
+    }
+    if (Reflect.has(this.handles, eventName)) {
+        EventsList = this.handles[eventName].eventsList;
+    } else {
+        this.handles[eventName] = {
+            eventsList: [callback],
+            isOne: true
+        };
+    }
+    EventsList.push(callback);
+};
+PubSub.prototype.notify = function (eventName) {
+    if (this.handles[eventName]) {
+        var EventsList = this.handles[eventName].eventsList,
+            i = 0,
+            isOne = this.handles[eventName].isOne;
+        if (EventsList) {
+            for (var _len = arguments.length, rest = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+                rest[_key - 1] = arguments[_key];
+            }
+
+            for (; i < EventsList.length; i++) {
+                EventsList[i].apply(this, rest);
+            }
+        }
+        if (isOne) {
+            this.unsubscribe(eventName);
+        }
+    }
+    return this;
+};
+PubSub.prototype.unsubscribe = function (eventName, callback) {
+    if (callback) {
+        var callbacks = this.handles[eventName].eventsList;
+        for (var i = 0; i < callbacks.length; i++) {
+            if (callbacks[i] === callback) {
+                this.handles[eventName].eventsList.splice(i, 1);
+                break;
+            }
+        }
+    } else {
+        delete this.handles[eventName];
+    }
+    return this;
+};
+
+exports.default = new PubSub();
---
+++ src/index.js
@@ -0,0 +1,71 @@
+/**
+ * Created by mapbar_front on 2018/10/16
+ */
+function PubSub() {
+    this.handles = {
+        eventName: {
+            eventsList: [],
+            isOne: false,
+        }
+    }
+}
+PubSub.prototype.subscribe = function (eventName, callback) {
+    let EventsList = [];
+    if (arguments.length < 2) {
+        throw new TypeError('arguments error');
+    }
+    if (Reflect.has(this.handles, eventName)) {
+        EventsList = this.handles[eventName].eventsList;
+    } else {
+        this.handles[eventName] = {
+            eventsList: [callback],
+            isOne: false,
+        };
+    }
+    EventsList.push(callback);
+}
+PubSub.prototype.subscribeOne = function (eventName, callback) {
+    let EventsList = [];
+    if (arguments.length < 2) {
+        throw new TypeError('arguments error');
+    }
+    if (Reflect.has(this.handles, eventName)) {
+        EventsList = this.handles[eventName].eventsList;
+    } else {
+        this.handles[eventName] = {
+            eventsList: [callback],
+            isOne: true,
+        };
+    }
+    EventsList.push(callback);
+}
+PubSub.prototype.notify = function (eventName, ...rest) {
+    if (this.handles[eventName]) {
+        let EventsList = this.handles[eventName].eventsList, i = 0, isOne = this.handles[eventName].isOne;
+        if (EventsList) {
+            for (; i < EventsList.length; i++) {
+                EventsList[i].apply(this, rest)
+            }
+        }
+        if (isOne) {
+            this.unsubscribe(eventName)
+        }
+    }
+    return this;
+}
+PubSub.prototype.unsubscribe = function (eventName, callback) {
+    if (callback) {
+        const callbacks = this.handles[eventName].eventsList;
+        for (let i = 0; i < callbacks.length; i++) {
+            if (callbacks[i] === callback) {
+                this.handles[eventName].eventsList.splice(i, 1);
+                break;
+            }
+        }
+    } else {
+        delete this.handles[eventName];
+    }
+    return this;
+}
+
+export default new PubSub();
